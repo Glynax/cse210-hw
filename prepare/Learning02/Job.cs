cb48c15335057527e62d244807b61d30a34b6e9f
Glynax <glynax@gmail.com>
2023-06-11 15:35:28 -0400

    Week 2 Learning

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,22 @@
+public class Job
+{
+    private string _company;
+    private string _jobTitle;
+    private int _startYear;
+    private int _endYear;
+
+    public Job(string company, string jobTitle, int startYear, int endYear)
+    {
+        _company = company;
+        _jobTitle = jobTitle;
+        _startYear = startYear;
+        _endYear = endYear;
+    }
+
+   public void DisplayJobDetails()
+    {
+    string jobDetails = $"{_jobTitle} ({_company}) {_startYear}-{_endYear}";
+    Console.WriteLine(jobDetails);
+    }
+
+}
