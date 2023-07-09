6a45bf628194cec6a04a9bc68434b84d5818b0b0
Glynax <glynax@gmail.com>
2023-07-08 21:36:30 -0400

    Final Project

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new List<Activity>();
+
+        RunningActivity running = new RunningActivity("Morning Run", 45, 5);
+        activities.Add(running);
+
+        SwimmingActivity swimming = new SwimmingActivity("Pool Workout", 60, 20);
+        activities.Add(swimming);
+
+        CyclingActivity cycling = new CyclingActivity("Afternoon Ride", 90, 15);
+        activities.Add(cycling);
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
